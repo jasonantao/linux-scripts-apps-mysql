bfa52eed3268723ab6bce0b7b2293f05a4fc0f3b
jasonantao <jasantao99@gmail.com>
2018-06-02 15:02:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
-# Setup the required environment
+# INSTALL JBOSS ON LINUX WITH REMOTE ACCESS AS A SERVICE
+
+# SETUP WILDFLY CONFIGURATION ENVIRONMENT AND PARAMETERS
 . ./env/setEnv.sh
 
-#------------------- INITIAL INSTALL --------------------
-yum install httpd -y
-echo "Server Available" > /var/www/html/index.html
-service httpd start
-chkconfig httpd on
+#CHECK IF WILDFLY INSTALLED AND RETURN IF INSTALLED
+. ./utils/exitIfInstalled.sh $wfHome
+
+# INSTALL REQUIRED DEVELOPMENT LIBRARIES
+./install//installJava8.sh
+
+# SET UP WILDFLY ADMIN USER
+./install/addPkgUser.sh $wfOwner $wfGroup $wfHome $pkg
+
+# DOWNLOAD AND INSTALL WILDFLY 10
+./install/installjBoss10.sh $wfOwner $wfGroup $wfHome $wfLog
+
+# SET UP WILDFLY REMOTING CONFIGURATION 
+./install/configjBossRemoting.sh $wfHome $wfAdmin
+
+# CONFIGURE WILDFLY AS A SERVICE AND START WILDFLY SERVICE
+./install/addjBossAsService.sh $wfHome 
+
+# START JBOSS WILDFLY
+service wildfly start
