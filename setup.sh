9c1a16d4f6e03d15b6aef4aae316eaa6bcae0b8c
jasonantao <jasantao99@gmail.com>
2018-06-05 10:42:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,52 @@
 #! /bin/bash
 # INSTALL MYSQL ON LINUX WITH REMOTE ACCESS AS A SERVICE
 
-# SETUP WILDFLY CONFIGURATION ENVIRONMENT AND PARAMETERS
+# SETUP MYSQL CONFIGURATION ENVIRONMENT AND PARAMETERS
 . ./env/setEnv.sh
 
+#Configuring MySQL for autmatic startup on reboot
+chkconfig mysqld on
+service mysqld start
+
+#mySQL user settings
+#mysqladmin -u root password [password] -y
+#mysqladmin -u root -p create [new_wp_db]
+
+#Creating a new user, 
+mysql -u root mysql -p
+
+#Creating database
+CREATE DATABASE wp_db
+
+#NOT SURE IF BELOW IS BEST WAY TO RUN OR OTHER WAY IS OF BEST PRACTICE> PLEASE ADVISE
+#CREATE USER 'owner'@'localhost' IDENTIFIED BY 'yourpassword';
+#GRANT ALL PRIVILEGES ON ideside.* TO 'ideuser'@'localhost';
+#FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON wp_db.* TO "owner"@"localhost" IDENTIFIED BY "password"
+FLUSH PRIVILEGES
+echo Privelges Flushed ~ mySQL Site 
+cd $sqlCurrDir
+#End of mySQLScript
+
+##############################################################################################################
 #CHECK IF WILDFLY INSTALLED AND RETURN IF INSTALLED
-. ./utils/exitIfInstalled.sh $wfHome
+#. ./utils/exitIfInstalled.sh $wfHome
 
 # INSTALL REQUIRED DEVELOPMENT LIBRARIES
-./install//installJava8.sh
+#./install//installJava8.sh
 
 # SET UP WILDFLY ADMIN USER
-./install/addPkgUser.sh $wfOwner $wfGroup $wfHome $pkg
+#./install/addPkgUser.sh $wfOwner $wfGroup $wfHome $pkg
 
 # DOWNLOAD AND INSTALL WILDFLY 10
-./install/installjBoss10.sh $wfOwner $wfGroup $wfHome $wfLog
+#./install/installjBoss10.sh $wfOwner $wfGroup $wfHome $wfLog
 
 # SET UP WILDFLY REMOTING CONFIGURATION 
-./install/configjBossRemoting.sh $wfHome $wfAdmin
+#./install/configjBossRemoting.sh $wfHome $wfAdmin
 
 # CONFIGURE WILDFLY AS A SERVICE AND START WILDFLY SERVICE
-./install/addjBossAsService.sh $wfHome 
+#./install/addjBossAsService.sh $wfHome 
 
 # START JBOSS WILDFLY
-service wildfly start
+#service wildfly start
