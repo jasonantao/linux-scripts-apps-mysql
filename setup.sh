0cba0e1ea3a7bb0faaa5758740d4b786d89966a1
jasonantao <jasantao99@gmail.com>
2018-06-16 19:08:58 -0400

    Update setup.sh
    New setup - fixes, should work, Testing in modularized form.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@
 #Configuring MySQL for autmatic startup on reboot
 #chkconfig mysqld on
 
-
 mysqluser=jason
 mysqlpass=jason
 mysqlhost=localhost
@@ -16,35 +15,27 @@ dbuser=jason
 dbpass=jason
 dbtable=${dbtable:-wp_}
 
-service mysqld start
+cd $HOME
+ls
+cd var
+ls
+cd lib
+ls
+cd mysql
+ls
+
+chown mysql:mysql -R /var/*
+chown mysql:mysql -R /var/lib/*
+chown mysql:mysql -R /var/lib/mysql/*
+
+chmod 755 -R /var/lib/mysql/*
+chmod 755 -R /var/lib/*
+
+service mysqld restart
+
 mysql -u root
 
 create database jason
-GRANT ALL PRIVILEGES ON jason.* TO jason@localhost IDENTIFIED BY 'jason';
+GRANT ALL PRIVILEGES ON jason.* TO root@localhost IDENTIFIED BY 'jason';
 FLUSH PRIVILEGES;
 exit
-
-mysql -u $mysqluser -p$mysqlpass -e "$dbsetup"
-mysql -u root jason
-show databases;
-
-#mySQL user settings
-#mysqladmin -u root password [password] -y
-#mysqladmin -u root -p create [new_wp_db]
-
-#Creating a new user, 
-#mysql -u root mysql -p
-
-#Creating database
-#CREATE DATABASE wp_db
-
-#NOT SURE IF BELOW IS BEST WAY TO RUN OR OTHER WAY IS OF BEST PRACTICE> PLEASE ADVISE
-#CREATE USER 'owner'@'localhost' IDENTIFIED BY 'yourpassword';
-#GRANT ALL PRIVILEGES ON ideside.* TO 'ideuser'@'localhost';
-#FLUSH PRIVILEGES;
-
-#GRANT ALL PRIVILEGES ON wp_db.* TO "owner"@"localhost" IDENTIFIED BY "password"
-#FLUSH PRIVILEGES
-#echo Privelges Flushed ~ mySQL Site 
-#cd $sqlCurrDir
-#End of mySQLScript
