ede3b0146e23f7c68e3e2651b6e6cc5b607b03c5
jasonantao <jasantao99@gmail.com>
2018-06-16 19:09:44 -0400

    Update mySQLBootrap.sh
    Testing Modularization Mock 30

--- mySQLBootrap.sh
+++ mySQLBootrap.sh
@@ -42,52 +42,5 @@ cd $installDir
 
 
 cd $HOME
-ls
-cd var
-ls
-cd lib
-ls
-cd mysql
-ls
 
-chown mysql:mysql -R /var/*
-chown mysql:mysql -R /var/lib/*
-chown mysql:mysql -R /var/lib/mysql/*
-
-chmod 755 -R /var/lib/mysql/*
-chmod 755 -R /var/lib/*
-
-service mysqld restart
-
-mysql -u root
-
-
-#./setup.sh
-
-
-#########################################
-## FOr testing purposes
-
-mysqluser=jason
-mysqlpass=jason
-mysqlhost=localhost
-dbname=jason
-dbuser=jason
-dbpass=jason
-dbtable=${dbtable:-wp_}
-
-#cd /tmp/scripts/db/mySQL
-
-#service mysqld start
-mysql -u root
-
-create database jason
-GRANT ALL PRIVILEGES ON jason.* TO root@localhost IDENTIFIED BY 'jason';
-FLUSH PRIVILEGES;
-exit
-
-mysql -u $mysqluser -p$mysqlpass -e "$dbsetup"
-mysql -u root jason
-show databases;
-
-
+./setup.sh
