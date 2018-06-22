a9a120dd667884fb3f7fc551998c842fb9fbcfc4
jasonantao <jasantao99@gmail.com>
2018-06-22 14:00:17 -0400

    Previous Code Inclusions
    echo "HERE 0"
    cd $HOME
    
    echo "HERE 2"
    ls
    cd var
    echo "HERE 3"
    ls
    cd lib
    ls
    echo "HERE 4"
    cd mysql
    ls
    
    echo "HERE 5"
    chown mysql:mysql -R /var/*
    chown mysql:mysql -R /var/lib/*
    chown mysql:mysql -R /var/lib/mysql/*
    
    chmod 755 -R /var/lib/mysql/*
    chmod 755 -R /var/lib/*
    
    service mysqld restart

--- setup.sh
+++ setup.sh
@@ -7,31 +7,9 @@
 #Configuring MySQL for autmatic startup on reboot
 #chkconfig mysqld on
 
-echo "HERE 0"
-cd $HOME
 
-echo "HERE 2"
-ls
-cd var
-echo "HERE 3"
-ls
-cd lib
-ls
-echo "HERE 4"
-cd mysql
-ls
 
-echo "HERE 5"
-chown mysql:mysql -R /var/*
-chown mysql:mysql -R /var/lib/*
-chown mysql:mysql -R /var/lib/mysql/*
-
-chmod 755 -R /var/lib/mysql/*
-chmod 755 -R /var/lib/*
-
-service mysqld restart
-
-mysql -u root
+mysql
 
 create database jason
 GRANT ALL PRIVILEGES ON root.* TO root@localhost IDENTIFIED BY 'root';
